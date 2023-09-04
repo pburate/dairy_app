74eb7f7c3dce0e8196fcb2b5e161f4b33477eaac
Piyushadcs <piyush.adcs@gmail.com>
2023-09-04 15:42:36 +0530

    All Project

---
+++ app/Views/Home/footer.php
@@ -0,0 +1,41 @@
+
+    <!-- jQuery -->
+    <script><?php include(APPPATH.'../public/assets/vendors/jquery/dist/jquery.min.js') ?></script>
+    <!-- Bootstrap -->
+    <script><?php include(APPPATH.'../public/assets/vendors/bootstrap/dist/js/bootstrap.bundle.min.js') ?></script>
+    <!-- FastClick -->
+    <script ><?php include(APPPATH.'../public/assets/vendors/fastclick/lib/fastclick.js')?></script>
+    <!-- NProgress -->
+    <script><?php include(APPPATH.'../public/assets/vendors/nprogress/nprogress.js')?></script>
+    <!-- Chart.js -->
+    <script><?php include(APPPATH.'../public/assets/vendors/Chart.js/dist/Chart.min.js')?></script>
+    <!-- gauge.js -->
+    <script><?php include(APPPATH.'../public/assets/vendors/gauge.js/dist/gauge.min.js')?></script>
+    <!-- bootstrap-progressbar -->
+    <script><?php include(APPPATH.'../public/assets/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js')?></script>
+    <!-- iCheck -->
+    <script><?php include(APPPATH.'../public/assets/vendors/iCheck/icheck.min.js')?></script>
+    <!-- Skycons -->
+    <script><?php include(APPPATH.'../public/assets/vendors/skycons/skycons.js')?></script>
+    <!-- Flot -->
+    <script><?php include(APPPATH.'../public/assets/vendors/Flot/jquery.flot.js')?></script>
+    <script><?php include(APPPATH.'../public/assets/vendors/Flot/jquery.flot.pie.js')?></script>
+    <script><?php include(APPPATH.'../public/assets/vendors/Flot/jquery.flot.time.js')?></script>
+    <script><?php include(APPPATH.'../public/assets/vendors/Flot/jquery.flot.stack.js')?></script>
+    <script><?php include(APPPATH.'../public/assets/vendors/Flot/jquery.flot.resize.js')?></script>
+    <!-- Flot plugins -->
+    <script><?php include(APPPATH.'../public/assets/vendors/flot.orderbars/js/jquery.flot.orderBars.js')?></script>
+    <script><?php include(APPPATH.'../public/assets/vendors/flot-spline/js/jquery.flot.spline.min.js')?></script>
+    <script><?php include(APPPATH.'../public/assets/vendors/flot.curvedlines/curvedLines.js')?></script>
+    <!-- DateJS -->
+    <script><?php include(APPPATH.'../public/assets/vendors/DateJS/build/date.js')?></script>
+    <!-- JQVMap -->
+    <script><?php include(APPPATH.'../public/assets/vendors/jqvmap/dist/jquery.vmap.js')?></script>
+    <script><?php include(APPPATH.'../public/assets/vendors/jqvmap/dist/maps/jquery.vmap.world.js')?></script>
+    <script><?php include(APPPATH.'../public/assets/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js')?></script>
+    <!-- bootstrap-daterangepicker -->
+    <script><?php include(APPPATH.'../public/assets/vendors/moment/min/moment.min.js')?></script>
+    <script><?php include(APPPATH.'../public/assets/vendors/bootstrap-daterangepicker/daterangepicker.js')?></script>
+
+    <!-- Custom Theme Scripts -->
+    <script><?php include(APPPATH.'../public/assets/build/js/custom.min.js')?></script>
---
+++ app/Views/index.php
@@ -0,0 +1,182 @@
+<?php include('Home/header.php') ?>
+
+
+
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+              <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <!-- menu profile quick info -->
+            <div class="profile clearfix">
+              <div class="profile_pic">
+                <img src="images/img.jpg" alt="..." class="img-circle profile_img">
+              </div>
+              <div class="profile_info">
+                <span>Welcome,</span>
+                <h2>John Doe</h2>
+              </div>
+            </div>
+            <!-- /menu profile quick info -->
+
+            <br />
+
+            <!-- sidebar menu -->
+            <?php include('Home/sidemenu.php') ?>
+            <!-- /sidebar menu -->
+
+          </div>
+        </div>
+
+        <!-- top navigation -->
+        <div class="top_nav">
+          <div class="nav_menu">
+              <div class="nav toggle">
+                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+              </div>
+              <nav class="nav navbar-nav">
+              <ul class=" navbar-right">
+                <li class="nav-item dropdown open" style="padding-left: 15px;">
+                  <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
+                    <img src="images/img.jpg" alt="">John Doe
+                  </a>
+                  <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item"  href="javascript:;"> Profile</a>
+                      <a class="dropdown-item"  href="javascript:;">
+                        <span class="badge bg-red pull-right">50%</span>
+                        <span>Settings</span>
+                      </a>
+                  <a class="dropdown-item"  href="javascript:;">Help</a>
+                    <a class="dropdown-item"  href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
+                  </div>
+                </li>
+
+                <li role="presentation" class="nav-item dropdown open">
+                  <a href="javascript:;" class="dropdown-toggle info-number" id="navbarDropdown1" data-toggle="dropdown" aria-expanded="false">
+                    <i class="fa fa-envelope-o"></i>
+                    <span class="badge bg-green">6</span>
+                  </a>
+                  <ul class="dropdown-menu list-unstyled msg_list" role="menu" aria-labelledby="navbarDropdown1">
+                    <li class="nav-item">
+                      <a class="dropdown-item">
+                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                        <span>
+                          <span>John Smith</span>
+                          <span class="time">3 mins ago</span>
+                        </span>
+                        <span class="message">
+                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                        </span>
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="dropdown-item">
+                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                        <span>
+                          <span>John Smith</span>
+                          <span class="time">3 mins ago</span>
+                        </span>
+                        <span class="message">
+                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                        </span>
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="dropdown-item">
+                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                        <span>
+                          <span>John Smith</span>
+                          <span class="time">3 mins ago</span>
+                        </span>
+                        <span class="message">
+                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                        </span>
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="dropdown-item">
+                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                        <span>
+                          <span>John Smith</span>
+                          <span class="time">3 mins ago</span>
+                        </span>
+                        <span class="message">
+                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                        </span>
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <div class="text-center">
+                        <a class="dropdown-item">
+                          <strong>See All Alerts</strong>
+                          <i class="fa fa-angle-right"></i>
+                        </a>
+                      </div>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <!-- /top navigation -->
+
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <!-- top tiles -->
+          <div class="row" style="display: inline-block;" >
+          <div class="tile_count">
+            <div class="col-md-2 col-sm-4  tile_stats_count">
+              <span class="count_top"><i class="fa fa-user"></i> Total Users</span>
+              <div class="count">2500</div>
+              <span class="count_bottom"><i class="green">4% </i> From last Week</span>
+            </div>
+            <div class="col-md-2 col-sm-4  tile_stats_count">
+              <span class="count_top"><i class="fa fa-clock-o"></i> Average Time</span>
+              <div class="count">123.50</div>
+              <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>3% </i> From last Week</span>
+            </div>
+            <div class="col-md-2 col-sm-4  tile_stats_count">
+              <span class="count_top"><i class="fa fa-user"></i> Total Males</span>
+              <div class="count green">2,500</div>
+              <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
+            </div>
+            <div class="col-md-2 col-sm-4  tile_stats_count">
+              <span class="count_top"><i class="fa fa-user"></i> Total Females</span>
+              <div class="count">4,567</div>
+              <span class="count_bottom"><i class="red"><i class="fa fa-sort-desc"></i>12% </i> From last Week</span>
+            </div>
+            <div class="col-md-2 col-sm-4  tile_stats_count">
+              <span class="count_top"><i class="fa fa-user"></i> Total Collections</span>
+              <div class="count">2,315</div>
+              <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
+            </div>
+            <div class="col-md-2 col-sm-4  tile_stats_count">
+              <span class="count_top"><i class="fa fa-user"></i> Total Connections</span>
+              <div class="count">7,325</div>
+              <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
+            </div>
+          </div>
+        </div>
+         
+          <br />
+
+        
+
+
+         
+        </div>
+        <!-- /page content -->
+
+        <!-- /footer content -->
+      </div>
+    </div>
+	<?php include('Home/footer.php') ?>
+  </body>
+</html>
