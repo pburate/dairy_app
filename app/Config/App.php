9b1807b749717a39c8e2280191c523d6f9a58065
Prachi Burate <prachi.adcs@gmail.com>
2023-09-05 12:36:58 +0530

    base_url changes

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost/dairy_app/dairy_app/';
+    public string $baseURL = 'http://localhost/dairy_app/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
