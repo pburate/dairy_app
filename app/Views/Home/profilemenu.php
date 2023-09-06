98ad19411fdd84e4b4527ea772952f69736af19a
Prachi Burate <prachi.adcs@gmail.com>
2023-09-06 17:57:48 +0530

    Css Updated

--- app/Views/Home/profilemenu.php
+++ app/Views/Home/profilemenu.php
@@ -4,21 +4,20 @@
     <div class="main_container">
       <div class="col-md-3 left_col">
         <div class="left_col scroll-view ">
-          <div class="navbar nav_title " style="border: 0;">
-            <a href="index.html" class="site_title"><img src="./public/assets/build/images/logo 2.png" alt="logo" class="logo_img"><span>Dairy Products!</span></a>
-          </div>
-
+          <!-- <div class="navbar nav_title " style="border: 0;">
+            <a href="index.html" class="site_title"><img src="./public/assets/build/images/logo 2.png" alt="logo" class="logo_img"></a>
+          </div> -->
           <div class="clearfix"></div>
 
- 
  <div class="profile clearfix">
               <div class="profile_pic">
-                <img src="./public/assets/build/images/profile_img.jpeg" alt="profile" class="img-circle profile_img">
+            <a href="index.php"><img src="./public/assets/build/images/logo 2.png" alt="logo" class="logo_img"></a>
+                
               </div>
               
               <div class="profile_info">
-                <!-- <h2>Welcome,</h2> -->
-                <h2 class="text-white" style="margin-left:-40px;">Welcome,<?php 
+                <br>
+                <h2 class="text-white" style="margin-left:-40px;"> Welcome, <?php 
                  $user = \auth()->user();
                 echo $user->username
                 ?>
