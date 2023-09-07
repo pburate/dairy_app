f7c460eb1be8b504dc29eb6242097d0c1d6d20e1
Prachi Burate <prachi.adcs@gmail.com>
2023-09-07 18:01:43 +0530

    product crud

--- app/Views/Home/sidemenu.php
+++ app/Views/Home/sidemenu.php
@@ -2,6 +2,7 @@
   <div class="menu_section">
     <!-- <h3>General</h3> -->
     <ul class="nav side-menu">
+<<<<<<< HEAD
     <li><a><i class="fa fa-edit"></i>Settings<span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
                       <li><a href="<?= base_url('AreasController') ?>">Areas</a></li>
@@ -9,7 +10,9 @@
                       <li><a href="<?= base_url('CustomersController') ?>">Customers</a></li>
                     </ul>
                   </li>
-      <li><a><i class="fa fa-home"></i>Products</a>
+    <li><a><i class="fa fa-table"></i>Settings</a>
+      </li>
+      <li><a href="ProductsController"><i class="fa fa-home"></i>Products</a>
       </li>
       <!-- <a href=""></a> -->
       <li><a><i class="fa fa-edit"></i>Customers</a></li>
