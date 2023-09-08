46cf8dc33a11fe3e88f5b1a3d46317e3bc77575c
Prachi Burate <prachi.adcs@gmail.com>
2023-09-08 12:00:03 +0530

    product model updates

--- app/Models/ProductsModel.php
+++ app/Models/ProductsModel.php
@@ -3,7 +3,7 @@ namespace App\Models;
 use CodeIgniter\Model;
 class ProductsModel extends Model
 {
-    protected $table = 'products';
+    protected $table = 'product';
     protected $primaryKey = 'product_id';
     protected $allowedFields = ['product_name','product_category' ,'weight', 'unit', 'selling_price_including_tax','tax_amount'];
 }
